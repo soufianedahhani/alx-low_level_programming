fcbc1188ee2f1f9a49c0e6e2b17933411251d1d0
soufiane dahhani <35426839+soufianedahhani@users.noreply.github.com>
2023-10-19 18:33:31 +0100

    Update 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,29 +1,20 @@
 #include "lists.h"
 
 /**
- * list_len - gets the number of nodes
+ * list_len - returns the number of elements in a linked lists
+ * @h: linked list of type listint_t to traverse
  *
- * @h: pointer to the head (first node)
- *
- * Return: number of nodes
+ * Return: number of elements in a linked list_t list.
  */
-
 size_t list_len(const list_t *h)
 {
-	size_t node_count = 1;
+	size_t num = 0;
 
-	/*if head is null return 0 nodes*/
-	if (h == NULL)
-		return (0);
-
-	/*iterate through the nodes*/
-	while (h->next != NULL)
+	while (h)
 	{
-		/*go to the next node*/
+		num++;
 		h = h->next;
-		/*count node*/
-		node_count++;
 	}
 
-	return (node_count);
+	return (num);
 }
