7354bab33330d10ee67e79309ca82a8332ff7ace
soufiane dahhani <35426839+soufianedahhani@users.noreply.github.com>
2023-10-19 18:36:18 +0100

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,26 +1,20 @@
-	#include "lists.h"
+#include "lists.h"
+
 
 /**
- * free_list - free single list link
- *
- * @head: pointer to the first node of the list
- *
- * Return: nothing
+ * free_list - Frees a list_t list
+ * @head: A pointer to the list_t list
  */
+
 void free_list(list_t *head)
 {
-	list_t *current;
+	list_t *tmp;
 
-	/**
-	 * while current head is not NULL
-	 * set head as the next node and
-	 * free the current node str then
-	 * free current node.
-	 */
-	while ((current = head) != NULL)
+	while (head)
 	{
-		head = head->next;
-		free(current->str);
-		free(current);
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
 	}
 }
